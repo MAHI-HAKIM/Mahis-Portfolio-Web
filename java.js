ac401a44d1823dc84c8e49a6e1ff10856b624a17
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-05-13 04:27:13 +0300

    completed home page

--- java.js
+++ java.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var typed = new Typed("#typed-text", {
+    strings: [
+      "a Website,UI/UX designer",
+      "a Photographer",
+      "a Videographer",
+      "a Gamer",
+    ],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop: true,
+  });
+});
+
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
