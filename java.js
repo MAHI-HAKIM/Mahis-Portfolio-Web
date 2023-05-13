dc80d9c6a5b06e2e87d1be6319e37ddaa324c44a
MAHI-HAKIM <103943510+MAHI-HAKIM@users.noreply.github.com>
2023-05-13 15:11:31 +0300

    Completed HomePage

--- java.js
+++ java.js
@@ -1,3 +1,29 @@
+// NAVBAR FEATURES
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("bx-x");
+  navbar.classList.toggle("open");
+};
+document.addEventListener("DOMContentLoaded", function () {
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 100 ||
+      document.documentElement.scrollTop > 100
+    ) {
+      document.querySelector("header").classList.add("scrolled");
+    } else {
+      document.querySelector("header").classList.remove("scrolled");
+    }
+  }
+});
+
+//AUTOMATIC TYPING FEATURE
 document.addEventListener("DOMContentLoaded", function () {
   var typed = new Typed("#typed-text", {
     strings: [
@@ -11,11 +37,3 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: true,
   });
 });
-
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
-
-menu.onclick = () => {
-  menu.classList.toggle("bx-x");
-  navbar.classList.toggle("open");
-};
